28676795467ad3b7b9f704cd2162aa2273bdf36b
Balint Kleri <balint@kleri.hu>
2014-04-10 18:45:40 +0200

    Allow modules to perform actions after a successful authorization

--- openid_connect.api.php
+++ openid_connect.api.php
@@ -32,5 +32,18 @@ function hook_openid_connect_LOGIN_PROVIDER_userinfo_alter(&$userinfo) {
 }
 
 /**
+ * Perform an action after a successful authorization.
+ *
+ * @param array $tokens
+ *   ID token and access token that we received as a result of the OpenID
+ *   Connect flow.
+ * @param string $destination
+ *   Destination path that was set prior to the OpenID Connect flow.
+ */
+function hook_openid_connect_post_authorize($tokens, $destination) {
+  drupal_set_message('Welcome back!');
+}
+
+/**
  * @} End of "addtogroup hooks".
  */

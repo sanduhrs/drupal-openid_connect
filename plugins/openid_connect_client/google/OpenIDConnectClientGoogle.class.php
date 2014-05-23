da996ef5ea8f99c88fea26c630a8ad7e963ce453
Bojan Zivanovic <bojanz@gmail.com>
2014-05-24 01:12:51 +0200

    Remove hook_openid_connect_LOGIN_PROVIDER_userinfo_alter(), move Google specific alter to its plugin.

--- plugins/openid_connect_client/google/OpenIDConnectClientGoogle.class.php
+++ plugins/openid_connect_client/google/OpenIDConnectClientGoogle.class.php
@@ -20,4 +20,21 @@ class OpenIDConnectClientGoogle extends OpenIDConnectClientBase {
       'userinfo' => 'https://www.googleapis.com/plus/v1/people/me/openIdConnect',
     );
   }
+
+  /**
+   * Overrides OpenIDConnectClientBase::retrieveUserInfo().
+   */
+  public function retrieveUserInfo($access_token) {
+    $userinfo = parent::retrieveUserInfo($access_token);
+    if ($userinfo) {
+      // For some reason Google returns the URI of the profile picture in a
+      // weird format: "https:" appears twice in the beginning of the URI.
+      // Using a regular expression matching for fixing it guarantees that
+      // things won't break if Google changes the way the URI is returned.
+      preg_match('/https:\/\/*.*/', $userinfo['picture'], $matches);
+      $userinfo['picture'] = $matches[0];
+    }
+
+    return $userinfo;
+  }
 }

6d17bf09482e69e2d0f6f5aeef29cf59de258c8c
Bojan Zivanovic <bojanz@gmail.com>
2014-05-24 00:56:59 +0200

    Add a generic plugin, primarily for use with Drupal servers.

---
+++ plugins/openid_connect_client/generic/OpenIDConnectClientGeneric.class.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Generic OpenID Connect client.
+ *
+ * Used primarily to login to Drupal sites powered by oauth2_server or PHP
+ * sites powered by oauth2-server-php.
+ */
+
+class OpenIDConnectClientGeneric extends OpenIDConnectClientBase {
+
+  /**
+   * Overrides OpenIDConnectClientBase::settingsForm().
+   */
+  public function settingsForm() {
+    $form = parent::settingsForm();
+
+    $default_site = 'https://example.com/oauth2';
+    $form['authorization_endpoint'] = array(
+      '#title' => t('Authorization endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('authorization_endpoint', $default_site . '/authorize'),
+    );
+    $form['token_endpoint'] = array(
+      '#title' => t('Token endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('token_endpoint', $default_site . '/token'),
+    );
+    $form['userinfo_endpoint'] = array(
+      '#title' => t('UserInfo endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->getSetting('userinfo_endpoint', $default_site . '/UserInfo'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * Overrides OpenIDConnectClientBase::getEndpoints().
+   */
+  public function getEndpoints() {
+    return array(
+      'authorization' => $this->getSetting('authorization_endpoint'),
+      'token' => $this->getSetting('token_endpoint'),
+      'userinfo' => $this->getSetting('userinfo_endpoint'),
+    );
+  }
+}

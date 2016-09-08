8f4164ae77a71fa3de3ad434cb9f3d51533f1ba1
sanduhrs <sanduhrs@28074.no-reply.drupal.org>
2016-09-08 09:41:14 +0200

    Issue #2796697 by frega, sanduhrs: Return $response object instead of executing $response->send in RedirectController::authenticate()

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -187,13 +187,11 @@ class RedirectController extends ControllerBase implements AccessInterface {
     // It's possible to set 'options' in the redirect destination.
     if (is_array($destination)) {
       $redirect = Url::fromUri('internal:/' . ltrim($destination[0], '/'), $destination[1])->toString();
-      $response = new RedirectResponse($redirect);
-      return $response->send();
+      return new RedirectResponse($redirect);
     }
     else {
       $redirect = Url::fromUri('internal:/' . ltrim($destination, '/'))->toString();
-      $response = new RedirectResponse($redirect);
-      return $response->send();
+      return new RedirectResponse($redirect);
     }
   }
 

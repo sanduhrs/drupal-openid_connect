dbed7c9f2579055252428fd451ae96038b05e61c
sanduhrs <sanduhrs@28074.no-reply.drupal.org>
2017-06-19 15:49:59 +0200

    Issue #2856900 by Sebastien @Actualys, sanduhrs: Add support for Linkedin, Facebook

---
+++ src/Plugin/OpenIDConnectClient/Linkedin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\openid_connect\Plugin\OpenIDConnectClient;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\openid_connect\Plugin\OpenIDConnectClientBase;
+
+/**
+ * Linkedin OpenID Connect client.
+ *
+ * Used primarily to login to Drupal sites powered by oauth2_server or PHP
+ * sites powered by oauth2-server-php.
+ *
+ * @OpenIDConnectClient(
+ *   id = "linkedin",
+ *   label = @Translation("Linkedin")
+ * )
+ */
+class Linkedin extends OpenIDConnectClientBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $url = 'https://www.linkedin.com/developer/apps';
+    $form['description'] = [
+      '#markup' => '<div class="description">' . $this->t('Set up your app in <a href="@url" target="_blank">my apps</a> on Facebook.', ['@url' => $url]) . '</div>',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEndpoints() {
+    return [
+      'authorization' => 'https://www.linkedin.com/oauth/v2/authorization',
+      'token' => 'https://www.linkedin.com/oauth/v2/accessToken',
+      'userinfo' => 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,picture-url)?format=json',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function authorize($scope = 'openid email') {
+    // Use Linkedin specific autorisations.
+    return parent::authorize('r_basicprofile r_emailaddress');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function retrieveUserInfo($access_token) {
+    $userinfo = parent::retrieveUserInfo($access_token);
+
+    if ($userinfo) {
+      $userinfo['email'] = $userinfo['emailAddress'];
+      $userinfo['sub'] = $userinfo['id'];
+      $userinfo['picture'] = $userinfo['pictureUrl'];
+
+      unset($userinfo['emailAddress'], $userinfo['id'], $userinfo['pictureUrl']);
+    }
+
+    return $userinfo;
+  }
+
+}

049f287add3586a584bd4de52c9dc02a4c6a08a8
sanduhrs <sanduhrs@28074.no-reply.drupal.org>
2017-09-25 15:53:22 +0200

    Issue #2905229 by sanduhrs: Restore custom path functionlity introduced in #2878694

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -188,7 +188,8 @@ class RedirectController extends ControllerBase implements AccessInterface {
 
     // It's possible to set 'options' in the redirect destination.
     if (is_array($destination)) {
-      $redirect = Url::fromUri('internal:/' . ltrim($destination[0], '/'), $destination[1])->toString();
+      $query = !empty($destination[1]['query']) ? '?' . $destination[1]['query'] : '';
+      $redirect = Url::fromUri('internal:/' . ltrim($destination[0], '/') . $query)->toString();
     }
     else {
       $redirect = Url::fromUri('internal:/' . ltrim($destination, '/'))->toString();
